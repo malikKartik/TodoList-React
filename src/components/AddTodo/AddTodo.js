6c2001abc4701d5adc1b37419ed9e3242ff03811
kartikmalik019 <kartikmalik019@gmail.com>
2020-04-19 12:49:32 +0530

    "Todo list using redux for state managment"

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import './AddTodo.css'
+import shid from 'shortid'
 const AddTodo = (props) =>{
-    const changeHandler = (e)=>{
+    const changeHandler = ()=>{
         const data = document.getElementById('todo-to-add').value
         document.getElementById('todo-to-add').value = ""
-        return(data)
+        props.dispatch({type: 'ADD_TODO',val:{title:data,completed: false,id:shid.generate()}})
     }
     return(
         <div id='add-todo-wraper'>
             <input type="text" id="todo-to-add"/>
-            <button onClick={()=>props.addTodo(changeHandler())} id='add-todo-btn'>add</button>
+            <button onClick={changeHandler} id='add-todo-btn'>add</button>
         </div>
     )
 }
 
-export default AddTodo
+const mapDispatchToProps = dispatch =>({dispatch})
+
+export default connect(null,mapDispatchToProps)(AddTodo)
